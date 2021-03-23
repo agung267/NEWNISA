855f06d34159814b02a2cc5c479327b68746226b
agung267 <63548223+agung267@users.noreply.github.com>
2021-03-24 04:40:32 +0700

    Update and rename locks_sql.py to locks_db.py

--- nisabot/module/no_sql/locks_db.py
+++ nisabot/module/no_sql/locks_db.py
@@ -1,5 +1,5 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/userbotindo/UserIndoBot.git>
+# Nisabot
+# Copyright (C) 2021 I Do Not Know, <https://github.com/agung267/NEWNISA.git>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,11 +15,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 # New chat added -> setup permissions
-import threading
 
-from sqlalchemy import Column, String, Boolean
-
-from ubotindo.modules.sql import SESSION, BASE
+from ubotindo.modules.no_sql import get_collection
 
 
 class Permissions(BASE):
