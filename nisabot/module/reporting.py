11110bcb2efe9e6b652be47434cb2332d88928f6
agung267 <63548223+agung267@users.noreply.github.com>
2021-03-23 22:37:13 +0700

    Update reporting.py

--- nisabot/module/reporting.py
+++ nisabot/module/reporting.py
@@ -1,5 +1,5 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/userbotindo/UserIndoBot.git>
+# Nisabot
+# Copyright (C) 2021 I Do Not Know, <https://github.com/agung267/NEWNISA.git>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -34,14 +34,14 @@ from telegram.ext import (
 )
 from telegram.utils.helpers import mention_html
 
-from ubotindo import LOGGER, dispatcher
-from ubotindo.modules.helper_funcs.alternate import typing_action
-from ubotindo.modules.helper_funcs.chat_status import (
+from nisabot import LOGGER, dispatcher
+from nisabot.modules.helper_funcs.alternate import typing_action
+from nisabot.modules.helper_funcs.chat_status import (
     user_admin,
     user_not_admin,
 )
-from ubotindo.modules.log_channel import loggable
-from ubotindo.modules.no_sql import get_collection
+from nisabot.modules.log_channel import loggable
+from nisabot.modules.no_sql import get_collection
 
 REPORT_GROUP = 5
 
