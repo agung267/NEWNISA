f080c4e6dbd5a849fb6c2da177841423206571f0
agung267 <63548223+agung267@users.noreply.github.com>
2021-03-23 23:23:38 +0700

    Update antiflood_sql.py

--- nisabot/module/sql/antiflood_sql.py
+++ nisabot/module/sql/antiflood_sql.py
@@ -1,5 +1,5 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/userbotindo/UserIndoBot.git>
+# Nisabot
+# Copyright (C) 2021 I Do Not Know, <https://github.com/agung267/NEWNISA.git>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@ import threading
 
 from sqlalchemy import String, Column, Integer, UnicodeText
 
-from ubotindo.modules.sql import SESSION, BASE
+from nisabot.modules.sql import SESSION, BASE
 
 DEF_COUNT = 0
 DEF_LIMIT = 0
