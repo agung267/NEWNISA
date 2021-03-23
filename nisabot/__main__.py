832272adf32bb7501c22e3561c203b3d30327666
agung267 <63548223+agung267@users.noreply.github.com>
2021-03-24 01:56:41 +0700

    Update __main__.py

--- nisabot/__main__.py
+++ nisabot/__main__.py
@@ -55,11 +55,11 @@ from nisabot import (
 
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
-from nisabot.modules import ALL_MODULES
-from nisabot.modules.helper_funcs.chat_status import is_user_admin
-from nisabot.modules.helper_funcs.filters import CustomFilters
-from nisabot.modules.helper_funcs.misc import paginate_modules
-from nisabot.modules.helper_funcs.alternate import typing_action
+from nisabot.module import ALL_MODULES
+from nisabot.module.helper_funcs.chat_status import is_user_admin
+from nisabot.module.helper_funcs.filters import CustomFilters
+from nisabot.module.helper_funcs.misc import paginate_modules
+from nisabot.module.helper_funcs.alternate import typing_action
 
 
 PM_START_TEXT = f"""
