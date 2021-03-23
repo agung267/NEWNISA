f59715603cf2a9f1724841e2a1b4726c717102a6
agung267 <63548223+agung267@users.noreply.github.com>
2021-03-23 23:10:14 +0700

    Update users_db.py

--- nisabot/module/no_sql/users_db.py
+++ nisabot/module/no_sql/users_db.py
@@ -1,5 +1,5 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/userbotindo/UserIndoBot.git>
+# Nisabot
+# Copyright (C) 2021 I Do Not Know, <https://github.com/agung267/NEWNISA.git>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -15,8 +15,8 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """User database utils."""
 
-from ubotindo import dispatcher
-from ubotindo.modules.no_sql import get_collection
+from nisabot import dispatcher
+from nisabot.modules.no_sql import get_collection
 
 
 USERS_DB = get_collection("USERS")
