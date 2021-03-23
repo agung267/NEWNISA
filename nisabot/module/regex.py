a1c28c0b4fd5f4088d28710215508247f70c4b2b
agung267 <63548223+agung267@users.noreply.github.com>
2021-03-23 22:35:19 +0700

    Update regex.py

--- nisabot/module/regex.py
+++ nisabot/module/regex.py
@@ -1,5 +1,5 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/userbotindo/UserIndoBot.git>
+# Nisabot
+# Copyright (C) 2021 I Do Not Know, <https://github.com/agung267/NEWNISA.git>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,8 +20,8 @@ import sre_constants
 import telegram
 from telegram.ext import Filters
 
-from ubotindo import LOGGER, dispatcher
-from ubotindo.modules.disable import DisableAbleMessageHandler
+from nisabot import LOGGER, dispatcher
+from nisabot.modules.disable import DisableAbleMessageHandler
 
 DELIMITERS = ("/", ":", "|", "_")
 
