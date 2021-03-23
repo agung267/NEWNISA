dbbc2911064e9774743d4a3cc0c638f7c753ab18
agung267 <63548223+agung267@users.noreply.github.com>
2021-03-23 22:28:56 +0700

    Update muting.py

--- nisabot/module/muting.py
+++ nisabot/module/muting.py
@@ -1,5 +1,5 @@
-# UserindoBot
-# Copyright (C) 2020  UserindoBot Team, <https://github.com/userbotindo/UserIndoBot.git>
+# Nisabot
+# Copyright (C) 2021 I Do Not Know, <https://github.com/agung267/NEWNISA.git>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,21 +22,21 @@ from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters
 from telegram.utils.helpers import mention_html
 
-from ubotindo import LOGGER, dispatcher
-from ubotindo.modules.helper_funcs.admin_rights import user_can_ban
-from ubotindo.modules.helper_funcs.alternate import typing_action
-from ubotindo.modules.helper_funcs.chat_status import (
+from nisabot import LOGGER, dispatcher
+from nisabot.modules.helper_funcs.admin_rights import user_can_ban
+from nisabot.modules.helper_funcs.alternate import typing_action
+from nisabot.modules.helper_funcs.chat_status import (
     bot_admin,
     can_restrict,
     is_user_admin,
     user_admin,
 )
-from ubotindo.modules.helper_funcs.extraction import (
+from nisabot.modules.helper_funcs.extraction import (
     extract_user,
     extract_user_and_text,
 )
-from ubotindo.modules.helper_funcs.string_handling import extract_time
-from ubotindo.modules.log_channel import loggable
+from nisabot.modules.helper_funcs.string_handling import extract_time
+from nisabot.modules.log_channel import loggable
 
 
 @bot_admin
